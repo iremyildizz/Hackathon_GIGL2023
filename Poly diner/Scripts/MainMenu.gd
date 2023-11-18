1458b3e1c93f4d49d3af8ecef9572e8500d4c8ba
Pablo <pablo.cabale@polymtl.ca>
2023-11-18 15:51:56 -0500

    Auto stash before merge of "WorkingBranch" and "origin/WorkingBranch"

--- Poly diner/Scripts/MainMenu.gd
+++ Poly diner/Scripts/MainMenu.gd
@@ -1,9 +1,8 @@
 extends Control
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$VBoxContainer/StartGameButton.grab_focus()
 
 func _on_start_game_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/MainGame.tscn")
