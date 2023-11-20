4e1f7e402d552657be41351f8ba2e7cfe9ed19e9
iremyildizz <iremyildiz02@gmail.com>
2023-11-20 18:49:30 -0500

    Food

---
+++ Poly diner/Scripts/CounterSpace.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+@onready var foodPositions : Array[Node2D] =  [
+	$SpawnContainer/FirstPlace, 
+	$SpawnContainer/SecondPlace, 
+	$SpawnContainer/ThirdPlace,
+	$SpawnContainer/ForthPlace, 
+	$SpawnContainer/FifthPlace
+]
+
+var plateCleanScene = preload("res://Scenes/Food.tscn") 
+var foodScenesList : Array[Node2D] = []
+
+
+func _on_new_food_timer_timeout():
+	print("time out")
+	if foodScenesList.size() < foodPositions.size():
+		instatiateFood(plateCleanScene)
+	else : 
+		$NewFoodTimer.stop()
+
+
+func instatiateFood(sceneType) -> void:
+	var newFoodScene = sceneType.instantiate()
+	foodPositions[foodScenesList.size()].add_child(newFoodScene)
+	foodScenesList.append(newFoodScene)
