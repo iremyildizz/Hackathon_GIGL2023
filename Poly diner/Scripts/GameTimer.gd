b37fce592b4c774c2f8826fc154218c83d22af33
iremyildizz <iremyildiz02@gmail.com>
2023-11-21 15:35:46 -0500

    Clock

--- Poly diner/Scripts/GameTimer.gd
+++ Poly diner/Scripts/GameTimer.gd
@@ -1,15 +1,35 @@
 extends Node2D
 
+@export var minutes : int = 0
+@export var seconds : int = 0
+var dsec : int = 0
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if seconds > 0 and dsec <= 0:
+		seconds -= 1
+		dsec = 10
+	if minutes > 0 and seconds <= 0: 
+		minutes -= 1
+		seconds = 60
+	
+	if seconds >= 10:
+		$sec.set_text(str(seconds))
+	else:
+		$sec.set_text("0" + str(seconds))
+	if minutes >= 10:
+		$sec.set_text(str(minutes))
+	else:
+		$sec.set_text("0" + str(minutes))
+		
+		
 
 
 func _on_game_timer_timeout():
-	pass # Replace with function body.
+	dsec -= 1
