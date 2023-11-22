ed37c9e503b62c0f02a72dec8b1ec4d5b19b5be7
iremyildizz <iremyildiz02@gmail.com>
2023-11-21 19:30:58 -0500

    Money randomize

--- Poly diner/Scripts/Money.gd
+++ Poly diner/Scripts/Money.gd
@@ -1,10 +1,28 @@
 extends Node2D
 
+enum MoneyType {MONEY = 0, COIN = 1}
+
+const possiblesVlues : int = 2
+var money : int = 0
+var image : Node2D = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	setRandomizedMoney()
 
+func setRandomizedMoney() -> void:
+	var randomValue = randi() % possiblesVlues
+	setMoney(randomValue)
+
+
+func setMoney(moneyValue : int) -> void:
+	if moneyValue == MoneyType.MONEY:
+		image = $Money
+	if moneyValue == MoneyType.COIN:
+		image = $Coin
+	
+	image.visible = true	
+	money = moneyValue
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
